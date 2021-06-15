946fb308c80306515825e562f6ac5da982bbec5d
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-06-14 23:29:55 -0700

    LRU Lib

--- src/cache-store.ts
+++ src/cache-store.ts
@@ -1,10 +1,12 @@
 import { ILogger } from 'the-logger';
+import * as LRU from 'lru-cache';
+
 
 export class CacheStore
 {
     constructor(logger : ILogger)
     {
-
+        const cache = new LRU.default();
     }
 
 }
