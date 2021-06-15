051c357f0fce1d701d589dc8e878f9ad3459f33c
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-06-15 00:15:40 -0700

    fetcher logic

--- src/cache-store.ts
+++ src/cache-store.ts
@@ -1,11 +1,53 @@
+import _ from 'the-lodash';
 import { ILogger } from 'the-logger';
-import * as LRU from 'lru-cache';
+import { Promise, Resolvable } from 'the-promise';
+import { default as LRUCache } from 'lru-cache';
 
-export class CacheStore
+export class CacheStore<K = any, V = any>
 {
+    private _cache : LRUCache<string, V>;
+
     constructor(logger : ILogger)
     {
-        const cache = new LRU.default();
+        this._cache = new LRUCache();
     }
 
+    set(key: K, value: V)
+    {
+        const k = this._rawKey(key);
+        this._cache.set(k, value);
+    }
+
+    get(key: K) : V | undefined
+    {
+        const k = this._rawKey(key);
+        return this._cache.get(k);
+    }
+
+    fetcher(cb: (key : K) => Resolvable<V>)
+    {
+        return {
+            get: (key: K) => {
+                const value = this.get(key);
+                if (!_.isUndefined(value)) {
+                    return Promise.resolve(value!);
+                }
+                return Promise.try(() => cb(key))
+                    .then(newValue => {
+                        console.log(newValue);
+                        this.set(key, newValue);
+                        return newValue;
+                    })
+            }
+        }
+    }
+
+    private _rawKey(key: K)
+    {
+        if (_.isString(key)) {
+            return key;
+        }
+        return _.stableStringify(key);
+    }
+     
 }
--- test/cache.ts
+++ test/cache.ts
@@ -12,7 +12,26 @@ describe("cache", function() {
     it('case1', function() {
 
         const cache = new CacheStore(logger);
-        
+        cache.set('foo', 'bar');
+
+        should(cache.get('foo')).be.equal('bar');
+
+        cache.set({'foo' : 'bar'}, 'bar1');
+        cache.set({'bar' : 'foo'}, 'bar2');
+
+        should(cache.get({'foo' : 'bar'})).be.equal('bar1');
+        should(cache.get({'bar' : 'foo'})).be.equal('bar2');
+    })
+
+    it('fetcher1', function() {
+
+        const cache = new CacheStore<string, string>(logger);
+        const fetcher = cache.fetcher((key) => `XX${key}`);
+
+        return fetcher.get('foo')
+            .then(value => {
+                should(value).be.equal('XXfoo');
+            });
     })
 
 });
